cfe8185c1719f051dcac0f67c521fc76db4f2df1
sonu <sonugopal41@gmail.com>
2017-07-17 00:35:37 +0530

    Ouput data from mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var Product=require('../models/product');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Cart' });
+    Product.find(function (err,docs) {
+        console.log(docs);
+        var productChunk=[];
+        var chunkSize=3;
+        for(var i=0;i<docs.length;i+=chunkSize){
+            productChunk.push(docs.slice(i,i+chunkSize));
+        }
+        res.render('shop/index', { title: 'Shopping Cart',products:productChunk});
+    })
+
 });
 
 module.exports = router;
